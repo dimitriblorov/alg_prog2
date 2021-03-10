ad84af95c5ad2b7cc5f78108506d4dcdd263abeb
dimitriblorov <dimitriblorov@gmail.com>
2021-03-10 12:38:45 -0300

    aula1

---
+++ listas.py
@@ -0,0 +1,5 @@
+lista_produtos = ['Café, Valor = 5,95 e Quantidade: 10 ', 'Açucar, Valor= 2,50 e Quantidade 5', 'Xícara, Valor= 3,75 e Quantidade 4']
+print('Lista de Produtos: ', lista_produtos)
+print (lista_produtos[0])
+del(lista_produtos[1])
+print(lista_produtos)
